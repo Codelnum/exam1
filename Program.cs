e5443d8c5d46c6e64d206961f1d4ea913fd1f04e
Codelnum <mvpugachev@mail.ru>
2022-06-19 20:10:25 +0300

    final version

--- Program.cs
+++ Program.cs
@@ -1,35 +1,40 @@
 ﻿using System;
 using static System.Console;
-Clear();
 
 WriteLine("Enter max string length");    
 int maxStrLength = int.Parse(ReadLine());
 WriteLine("Enter array size");
-int arrSize = int.Parse(ReadLine()); 
+int startArraySize = int.Parse(ReadLine()); 
 int sortLength = 3;                                 //отбор по длине строки n.
 
-string [] resultArr = new string [arrSize];
-int count =0;                               //счетчик кол-ва строк <= n и размера итогового массива.
-int j =0;
-
-string[]randomArr = new string[arrSize];
-    for (int i = 0; i < arrSize; i++)
+string[]startArray = new string[startArraySize];
+int count = 0;                                      //счетчик кол-ва строк с длиной <= n и размер итогового массива.
+    for (int i = 0; i < startArraySize; i++)
     {   
         int strLength = new Random().Next(1,maxStrLength);
         string arrString = new string(GetString(strLength));
-        randomArr[i]= arrString;
+        startArray[i]= arrString;
         if(strLength<=sortLength)
         {
             count++;
-            resultArr[j]=randomArr[i];
-            j++;
         }        
     }
 
+string [] resultArray = new string [count];
+int j = 0;                                  
+for (int i = 0; i <startArraySize; i++)
+{
+    if (startArray[i].Length<=sortLength)
+    {
+        resultArray[j]= startArray[i];
+        j++;
+    }
+}
+
+
 WriteLine($"START ARRAY:");
-PrintArray(randomArr,arrSize);
-WriteLine();
-WriteLine();
+PrintArray(startArray,startArraySize); WriteLine();WriteLine();
+
 
 if(count == 0)
 {
@@ -37,9 +42,11 @@ WriteLine($"В массиве нет строк, соответвующих ус
 }
 else
 {
-WriteLine($"RESULT ARRAY:");
-PrintArray(resultArr, count);
+WriteLine($"RESULT ARRAY(size = {count}):");
+PrintArray(resultArray, count);
 }
+
+
 
 
 
